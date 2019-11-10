cb0d7418f91d2f8046f9e294dd09fde3c0f5f796
chen <3160495815@qq.com>
2019-11-10 23:17:48 +0800

    作业完成

--- 工具类.js
+++ 工具类.js
@@ -1,3 +1,4 @@
+//随机十六进制颜色
 function Sixteencolor(){
             var arr = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
             var color = '#';
@@ -5,4 +6,18 @@ function Sixteencolor(){
                 color += arr[parseInt(Math.random()*16)];
             }
             return color;
-        }
+}
+//随机整数
+function randomInt(m,n){
+    var num = parseInt(Math.random()*(n - m + 1) + m );
+    return num;
+}
+//随机rgb颜色
+function rgbColor(){
+    var r = parseInt(Math.random()*256);
+    var g = parseInt(Math.random()*256);
+    var b = parseInt(Math.random()*256);
+    var color = 'rgb('+r+','+g+','+b+')';
+    return color;
+
+}
